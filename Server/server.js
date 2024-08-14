0ea56cde8060626277b34c10fd004569f52b75b6
vivek <122478977+vivek1523@users.noreply.github.com>
2024-08-14 13:00:23 +0500

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -23,8 +23,7 @@ app.use(cors({
 app.use('/', userRouter);
 app.use("/", categoryRouter);
 app.use("/", transactionRoute) 
-
-app.use(express.static(path.join(__dirname, 'public')));
+ 
 
 app.use(errorHandler);
 
